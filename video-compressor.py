e8ae38134dee690fc0dd1ac6d6d1cd0f16fd96a4
TCsTheMechanic <ceruttioliveira@hotmail.com>
2022-08-04 12:28:41 -0300

    Add support to cude cores

--- video-compressor.py
+++ video-compressor.py
@@ -8,7 +8,8 @@ os.chdir(videosLocation)
 
 for videoName in videosNames:
   subprocess.call([
-    'ffmpeg', '-i', videoName, '-vcodec', 'h264', '-acodec', 'mp2', '-vb',
-    '7M', '-maxrate', '9M', '-minrate', '5M', '-bufsize', '6M', f'Video{count}.mp4'
+    'ffmpeg', '-hwaccel', 'cuda', '-i', videoName, '-vcodec', 'h264',
+    '-acodec', 'mp2', '-vb', '7M', '-maxrate', '9M', '-minrate', '5M',
+    '-bufsize', '6M', f'Video{count}.mp4'
   ])
   count += 1
